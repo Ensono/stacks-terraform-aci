6a94be65ee95ea1752488289b6db64c3457725cd
Dan Chambers <daniel.chambers@ensono.com>
2024-05-08 14:26:27 +0100

    Adding outputs to the stacks-infrastructure-aci terraform for test use later

---
+++ deploy/terraform/outputs.tf
@@ -0,0 +1,39 @@
+output "acg_name" {
+  value = module.aci.container_group_name
+}
+
+output "acg_id" {
+  value = module.aci.container_group_id
+}
+
+output "acg_location" {
+  value = module.aci.container_group_location
+}
+
+output "acg_resource_group_name" {
+  value = module.aci.container_group_resource_group_name
+}
+
+output "acg_os_type" {
+  value = module.aci.container_group_os_type
+}
+
+output "acg_sku" {
+  value = module.aci.container_group_sku
+}
+
+output "acg_ip_address" {
+  value = module.aci.container_group_ip_address
+}
+
+output "acg_subnet_ids" {
+  value = module.aci.container_group_subnet_ids
+}
+
+output "acg_ports" {
+  value = module.aci.container_group_ports
+}
+
+output "acg_tags" {
+  value = module.aci.container_group_tags
+}
